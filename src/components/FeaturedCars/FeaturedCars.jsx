f0bdbbd6b3a176e7f3a3bfa2beef9141196ec8b2
Fábio <fabiosgcontato@gmail.com>
2023-06-27 19:42:16 -0300

    FeaturedCars, list now just show 6 elements at all

--- src/components/FeaturedCars/FeaturedCars.jsx
+++ src/components/FeaturedCars/FeaturedCars.jsx
@@ -3,8 +3,21 @@ import { Description, HeaderCont, TagTitle, Title } from '../../globalStyle';
 import AdvertiseCard from '../AdvertiseCard/AdvertiseCard';
 import ButtonArrowCta from '../Buttons/ButtonArrowCta/ButtonArrowCta';
 import { CarsContainer, Wrapper } from './FeaturedCars.styles';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function FeaturedCars({ carsList, loading }) {
+
+  const [cars, setCars] = useState([]);
+
+
+  useEffect(() => {
+    if (carsList) {
+      for (let i = 0; i < 6; i++) {
+        setCars(current => [...current, carsList[i]]);
+      }
+    }
+  }, [carsList])
 
   return (
     <Wrapper>
@@ -12,19 +25,19 @@ export default function FeaturedCars({ carsList, loading }) {
         <TagTitle>Car List</TagTitle>
         <Title>Find the Best Deals For You</Title>
         <Description>
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br/>Aut natus ad pariatur dolorem aliquam nemo perferendis dignissimos vel ullam fugit?
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />Aut natus ad pariatur dolorem aliquam nemo perferendis dignissimos vel ullam fugit?
         </Description>
       </HeaderCont>
       <CarsContainer>
         {loading && <p>Carregando...</p>}
-        {carsList &&
-          carsList.map((advertise, key) => (
+        {cars &&
+          cars.map((advertise, key) => (
             <AdvertiseCard
               advertise={advertise}
               key={key}
             />
           ))}
-        {carsList && carsList.lenght === 0 && (
+        {cars && cars.length === 0 && (
           <p>Não foram encontrados anuncios...</p>
         )}
       </CarsContainer>
